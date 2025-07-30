1d8b752fa9e60109bd609c6245492249793fee68
Liz Tan <elizabethsztan@gmail.com>
2025-07-30 10:15:17 +0100

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interpretsr",
-    version="1.0.0",
+    version="1.0.1",
     author="InterpretSR Team",
     description="Neural Networks with Symbolic Regression for Interpretable Machine Learning",
     long_description=long_description,
