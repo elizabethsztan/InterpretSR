46cc717e10cf33091addcfdd90cb61dc68469ad6
Liz Tan <elizabethsztan@gmail.com>
2025-07-30 10:26:09 +0100

    package restructuring

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,1 @@
-"""
-Core InterpretSR modules
-"""
-
-from .mlp_sr import MLP_SR
-from .utils import load_existing_weights_auto
-
-__all__ = [
-    "MLP_SR",
-    "load_existing_weights_auto"
-]
+# Empty init file for src directory
