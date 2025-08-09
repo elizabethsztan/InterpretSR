67ca550023fdc64c8b14f27f2740fd190e76141c
Liz Tan <elizabethsztan@gmail.com>
2025-08-09 14:29:42 +0800

    update pypi with newest version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="torch-symbolic",
-    version="1.0.1",
+    version="1.0.2",
     author="Liz Tan",
     author_email= "eszt2@cam.ac.uk",
     description="Deep Learning Interpretability with Symbolic Regression.",
